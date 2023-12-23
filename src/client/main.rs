3a056da9680970e2f73270b0e97148cbf589f0c0
Andreas Bank <andreas.bank@n3rd.se>
2023-12-23 20:52:39 +0100

    Refactor to move the server parts into its own module

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,10 +1,18 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
+use std::sync::{Mutex};
 use std::str::from_utf8;
+use once_cell::sync::Lazy;
 
+use liblink::common::verbose_print::{VerbosityLevel, Verbosity};
+use liblink::{Error, Inform, Detail, Spam};
+
+static VERBOSITY: Lazy<Mutex<Verbosity>> = Lazy::new(|| Mutex::new(Verbosity::new()));
 static MAGIC_BYTES: [u8; 4] = [ 0xAB, 0xBA, 0xAB, 0xBA ];
 
 fn main() {
+    VERBOSITY.lock().unwrap().set_level(VerbosityLevel::Spam);
+
     match TcpStream::connect("localhost:65432") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 65432");
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,75 +1,18 @@
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{Read, Write};
-use std::result::Result;
 use std::sync::{Mutex};
-use debug_print::debug_println;
+use std::error::Error;
+//use debug_print::debug_println;
 use clap::{arg, command};
 use once_cell::sync::Lazy;
 
-use liblink::common::utils::encode_hex;
 use liblink::common::verbose_print::{VerbosityLevel, Verbosity};
 use liblink::{Error, Inform, Detail, Spam};
+mod server;
+use server::LinkServer;
 
 static VERBOSITY: Lazy<Mutex<Verbosity>> = Lazy::new(|| Mutex::new(Verbosity::new()));
 
-static MAGIC_BYTES: [u8; 4] = [ 0xAB, 0xBA, 0xAB, 0xBA ];
-
-fn authenticate(data: &[u8]) -> Result<(), &'static str> {
-    debug_println!("Magic bytes received: {}", encode_hex(data));
-    if MAGIC_BYTES == data { return Ok(()); }
-
-    Err("Wrong magic bytes")
-}
-
-fn handle_accept(mut stream: TcpStream) {
-    let mut authenticated = false;
-    let mut data = [0 as u8; 1024];
-
-    loop {
-        match stream.read(&mut data) {
-
-            Ok(size) if size > 0 => {
-
-                /* Check for the magic bytes */
-                if !authenticated {
-                    match authenticate(&data[0..MAGIC_BYTES.len()]) {
-                        Ok(_) => {
-                            authenticated = true;
-                            println!("Authentication successful");
-                        }
-                        Err(errstr) => {
-                            println!("Failed to authenticate: {}", errstr);
-                            break;
-                        }
-                    }
-                }
-
-                /* echo the data */
-                println!("Sending data back");
-                stream.write(&data[4..size]).unwrap();
-            },
-
-            /* If we received 0 bytes, we're done */
-            Ok(_) => {
-                println!("Gracefully closing the connection with {}", stream.peer_addr().unwrap());
-                break;
-            },
-
-            Err(_) => {
-                println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-                stream.shutdown(Shutdown::Both).unwrap();
-                break;
-            }
-        }
-    }
-}
-
-fn main() {
-    let addr = "127.0.0.1";
-    let port = 65432;
-    let listener = TcpListener::bind((addr, port)).unwrap();
-
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut listener = LinkServer::new(&"127.0.0.1".to_string(), 65432).unwrap();
     let cmd_args = command!().args(&[
         arg!(-d --debug <lvl> "Enable debug at a certain level"),
         arg!(-v --verbose ... "Verbose mode. Can be specified up to 3 times"),
@@ -98,18 +41,10 @@ fn main() {
     Detail!("Parsed conf: '{}')", _listen_conf);
 
     println!("Listening on port 65432");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("Accepted connection from address {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    handle_accept(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
-    drop(listener);
+    listener.start()?;
+
+    // This will never be reached
+    listener.stop();
+
+    Ok(())
 }
